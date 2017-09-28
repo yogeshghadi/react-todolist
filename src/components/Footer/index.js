291330c8913574e0d05922c91a9e78b54f256d44
yogeshghadi <yogesh.ghadigaonkar@digitaslbi.com>
2017-09-28 15:49:23 +0530

    removed / from route path

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,15 +7,15 @@ const Footer = (props) => (
   <div className="row todo-footer">
 
     <div className="active-number col-xs-4">
-      <Link to="/#active" className={ (props.nowShowing === "active") ? "selected" : ""} >
+      <Link to="#active" className={ (props.nowShowing === "active") ? "selected" : ""} >
         <span>{props.activeNumber} items left</span>
       </Link>
     </div>
 
     <div className="filters col-xs-8">
-      <Link to="/#" className={ (props.nowShowing === "all") ? "selected" : ""} >All</Link>
-      <Link to="/#active" className={ (props.nowShowing === "active") ? "selected" : ""} >Active</Link>
-      <Link to="/#completed" className={ (props.nowShowing === "completed") ? "selected" : ""} >Completed</Link>
+      <Link to="#" className={ (props.nowShowing === "all") ? "selected" : ""} >All</Link>
+      <Link to="#active" className={ (props.nowShowing === "active") ? "selected" : ""} >Active</Link>
+      <Link to="#completed" className={ (props.nowShowing === "completed") ? "selected" : ""} >Completed</Link>
     </div>
     
   </div>
